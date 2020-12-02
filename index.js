e87c54edfd548782fbce60935b9c793a8250b6bc
haingo <xurixiru2011@gmail.com>
2020-12-02 00:12:46 -0800

    add jenkins

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => { // new
-  res.send('Homepage! Hello world.');
+  res.send('Homepage! Hello world. Running inside a container');
 });
 
 app.listen(3000, () => console.log('listening on port 3000'));
